9d506834879a808c5eee5b4d3fa9604f48156e8a
kjirou <sorenariblog@gmail.com>
2019-03-11 23:59:20 +0900

    Show a React Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('www hello world')
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Root = () => {
+  return <div>Hello, React!</div>
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root />, document.querySelector('.js-app'));
+});
