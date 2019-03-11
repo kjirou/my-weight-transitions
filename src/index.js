711b14f603b46f992557327c94513f2e28277465
kjirou <sorenariblog@gmail.com>
2019-03-12 00:27:07 +0900

    Express weight transitions as a graph

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const {
+  CartesianGrid,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+} = require('recharts');
+
+const weightTransitions = require('../weight-transitions.json');
+
+const WeightTransitionsGraph = () => {
+  const data = weightTransitions.weightTransitions.map(weightTransition => {
+    return {
+      weight: weightTransition.weight,
+      date: weightTransition.date,
+    };
+  });
+
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="date"/>
+      <YAxis/>
+    </LineChart>
+  );
+};
 
 const Root = () => {
-  return <div>Hello, React!</div>
+  return <WeightTransitionsGraph />;
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Root />, document.querySelector('.js-app'));
+  ReactDOM.render(
+    React.createElement(Root, {}),
+    document.querySelector('.js-app')
+  );
 });
